805626fcb62d49bd63cdc45d650ed53da3610d51
mdowais99 <mdowais905@gmail.com>
2022-09-28 17:31:50 +0500

    first

---
+++ app.js
@@ -0,0 +1,18 @@
+function shapeChanger() {
+    // var shapeCh = document.getElementById('shapeee');
+    // console.log(shapeCh)
+    // shapeCh.style.borderRadius="30px;";
+
+    document.getElementById('shapeee').style.borderRadius = '2px';
+}
+
+function shapechan() {
+    document.getElementById('shapeee').style.borderRadius = "50%";
+}
+
+function shapecha() {
+    document.getElementById('shapeee').style.borderRadius = "91% 9% 90% 10% / 91% 10% 90% 9% ";
+}
+function shapechar() {
+    document.getElementById('shapeee').style.borderRadius = "84% 16% 58% 42% / 63% 17% 83% 37% ";
+}
